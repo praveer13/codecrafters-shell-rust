2f98276ae9e2eb3cce436aa66b7bae12d7c89395
Praveer Naigaonkar <naigap@amazon.com>
2025-10-20 22:45:29 -0700

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,5 @@ fn main() {
     // Wait for user input
     let mut command = String::new();
     io::stdin().read_line(&mut command).unwrap();
+    println!("{}: command not found", command.trim());
 }
