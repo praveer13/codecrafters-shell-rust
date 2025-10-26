d2c3f46be31b2ce168fcb57546bce8fee2246293
Praveer Naigaonkar <pravs.13@gmail.com>
2025-10-26 13:18:57 -0700

    Fix exec name

--- src/main.rs
+++ src/main.rs
@@ -60,7 +60,7 @@ fn main() {
                 }
             }
             _ => {
-                let file_path_buf = find_executable(command);
+                let file_path_buf = find_executable(parts[0]);
                 if let Some(file_path) = file_path_buf {
                     let mut child = process::Command::new(file_path)
                         .args(&parts[1..])
